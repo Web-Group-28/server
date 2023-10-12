11c72c3c3862ee9b14aaf193c91ebb876e87583b
unknown <trieu81200111@gmail.com>
2023-10-12 23:15:55 +0700

    remove duplicate index file

--- server/config/db/index.js
+++ server/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27018', {
+        await mongoose.connect('mongodb://127.0.0.1:27017', {
            
             useNewUrlParser: true,
             useUnifiedTopology: true,
--- server/index.js
+++ server/index.js
@@ -1,17 +1,40 @@
-const http = require('http');
 const db = require('./config/db');
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
 
 db.connect();
- 
-const hostname = '0.0.0.0';
-const port = 3000;
- 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(morgan('combined'));
+app.use(express.json());
+
+app.use(express.static(__dirname + '/static'));
+app.use(express.urlencoded({ extended: true }));
+app.set('port', 3000);
+
+app.get('/', (req, res) => {
+  //TODO
 });
- 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+app.post('/api/register', require('./routes/api/register'));
+app.post('/api/login', require('./routes/api/login'));
+app.get('/api/user/profile', require('./routes/api/profile'));
+app.get('/api/languages', require('./routes/api/languages'));
+app.get('/api/courses', require('./routes/api/courses'));
+app.get('/api/courses/:courseId/lessons', require('./routes/api/lessons'));
+app.get('/api/courses/:courseId/lessons/:lessonId', require('./routes/api/lesson'));
+app.get('/api/user/progress', require('./routes/api/progress'));
+app.get('/api/courses/:courseId/lessons/:lessonId/exercises', require('./routes/api/exercises'));
+app.get('/api/courses/:courseId/lessons/:lessonId/exercises/:exerciseId', require('./routes/api/exercise'));
+app.post('/api/courses/:courseId/lessons/:lessonId/exercises/:exerciseId/submit', require('./routes/api/submit'));
+app.get('/api/leaderboard', require('./routes/api/leaderboard'));
+app.get('/api/user/friends', require('./routes/api/friends'));
+
+app.listen(app.get('port'), () => {
+   console.log(`Node app is running on port ${app.get('port')}`);
 });
