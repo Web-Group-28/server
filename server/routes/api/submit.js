d2cac847be1716052999ae3b68e48ee1ed3faac8
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-11-22 22:38:19 +0700

    add session to app

--- server/routes/api/submit.js
+++ server/routes/api/submit.js
@@ -1,13 +1,16 @@
 const { default: mongoose } = require('mongoose');
 const History = require('../../models/history');
+const { default: axios } = require('axios');
+
 /**
  * Get user id in the current session
  * @returns
  */
-function getUserID() {
-   //TODO
-   return "GUEST";
+async function getUserID() {
+   const session = await axios.get('http://localhost:3000/api/session');
+   return session.data.userID;
 }
+
 
 /**
  * Submit
@@ -17,7 +20,7 @@ function getUserID() {
 const submit = async (req, res) => {
    const courseId = String(req.params.courseId);
    const lessonId = String(req.params.lessonId);
-   const userID = getUserID();
+   const userID = await getUserID();
    const learnedIDs = (await History.findOne({
       userID: userID
    }).exec()).learnedID;
