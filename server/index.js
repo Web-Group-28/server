06f591ec4cd1a4995e58120bf91f9be1ad6d244b
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-12-10 15:15:40 +0700

    refactor API

--- server/index.js
+++ server/index.js
@@ -47,7 +47,6 @@ app.get('/courses/:courseId/lessons', async (req, res) => {
 app.get('/api/user/profile', require('./routes/api/profile'));
 app.get('/api/user/progress', require('./routes/api/progress'));
 app.get('/api/courses/:courseId/lessons/:lessonId/exercises/:exerciseId', require('./routes/api/exercise'));//?
-app.post('/api/courses/:courseId/lessons/:lessonId/submit', require('./routes/api/submit'));//OK
 app.get('/api/leaderboard', require('./routes/api/leaderboard'));
 app.get('/api/user/friends', require('./routes/api/friends'));
 
--- server/routes/api/courses.js
+++ server/routes/api/courses.js
@@ -3,11 +3,13 @@ const courseController = require('../../controllers/courseController');
 const lessonsController = require('../../controllers/lessonsController');
 const lessonController = require('../../controllers/lessonController');
 const exercisesController = require('../../controllers/exercisesController');
+const submitController = require('../../controllers/submitController');
 const router = express.Router();
 
 router.get('/', courseController.courses);//OK
 router.get('/:courseId/lessons', lessonsController.lessons);//OK
 router.get('/:courseId/lessons/:lessonId', lessonController.lesson);//OK
 router.get('/:courseId/lessons/:lessonId/exercises', exercisesController.exercises);//OK
+router.post('/:courseId/lessons/:lessonId/submit', submitController.submit);//OK
 
 module.exports = router;
