4b250d7a30407383f05ae0644e26f06d75cbeb61
Phung Nam <nampp@cystack.net>
2023-12-14 00:05:08 +0700

    add /profile to get weekScore

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+// middleware
+const middleware = require("../../middlewares/auth");
+
 // controllers
 const userController = require("../../controllers/userController");
 
-router.post("/profile", userController.getProfile);
+router.get("/", middleware, userController.getWeekScore);
 
 module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 const languagesRouter = require('./api/languages');
 const authRouter = require('./api/user');
 const courseRouter = require('./api/courses');
+const userRouter = require('./api/profile')
 
 function route(app) {
     app.use('/api/languages/', languagesRouter);
     app.use('/api/courses/', courseRouter);
     app.use('/api', authRouter);
+    app.use('/api/profile', userRouter)
 }
 
 module.exports = route;
