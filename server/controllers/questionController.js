5cc42e1bfa41a3609400f81f3a7a8f1e5290b96d
Nguyễn Tiến Nam <trieu81200111@gmail.com>
2023-12-23 22:16:15 +0700

    Added get question

--- server/controllers/questionController.js
+++ server/controllers/questionController.js
@@ -57,6 +57,48 @@ class QuestionController{
             return res.status(400).send(BaseResponse.ofError(err, 400))
         }
     }
+
+    async getAllChoiceQuestion(req, res){
+        try{
+            const size = req.query.size || 10;
+            const page = req.query.page || 0;
+            const questions = await ChoiceQuestion.find({})
+                .skip(page * size).limit(size).exec();
+            const count = await ChoiceQuestion.count();
+            return res.status(200).send(BaseResponse.ofSucceed(questions, page, size, count));
+        }
+        catch(err){
+            return res.status(400).send(BaseResponse.ofError(err, 400))
+        }
+    }
+
+    async getAllSentenceQuestion(req, res){
+        try{
+            const size = req.query.size || 10;
+            const page = req.query.page || 0;
+            const questions = await SentenceQuestion.find({})
+                .skip(page * size).limit(size).exec();
+            const count = await SentenceQuestion.count();
+            return res.status(200).send(BaseResponse.ofSucceed(questions, page, size, count));
+        }
+        catch(err){
+            return res.status(400).send(BaseResponse.ofError(err, 400))
+        }
+    }
+    
+    async getAllFillQuestion(req, res){
+        try{
+            const size = req.query.size || 10;
+            const page = req.query.page || 0;
+            const questions = await FillQuestion.find({})
+                .skip(page * size).limit(size).exec();
+            const count = await FillQuestion.count();
+            return res.status(200).send(BaseResponse.ofSucceed(questions, page, size, count));
+        }
+        catch(err){
+            return res.status(400).send(BaseResponse.ofError(err, 400))
+        }
+    }
 }
 
 module.exports = new QuestionController();
--- server/routes/api/question.js
+++ server/routes/api/question.js
@@ -5,5 +5,8 @@ const questionController = require("../../controllers/questionController")
 router.post("/choice", questionController.createChoiceQuestion);
 router.post("/sentence", questionController.createSentenceQuestion);
 router.post("/fill", questionController.createFillQuestion);
+router.get("/choice", questionController.getAllChoiceQuestion);
+router.get("/sentence", questionController.getAllSentenceQuestion);
+router.get("/fill", questionController.getAllFillQuestion);
 
 module.exports = router;
